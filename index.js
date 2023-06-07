eedbaa1a4361a10af686cb5d8fef4906c667e8a0
Sascha-davidson <saschadavidson1@gmail.com>
2023-06-07 14:51:51 +0200

    debriefing beter gemaakt

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.set("views", "./views");
 
 app.use(express.static("public"));
 
-app.get('/', function (request, response) {
-    response.render('index')
-  })
+app.get("/", function (request, response) {
+  response.render("index");
+});
 
 app.set("port", process.env.PORT || 7000);
-app.listen(app.get('port'), function () {
-    console.log(`application started on http://localhost:${app.get('port')}`)
-  })
+app.listen(app.get("port"), function () {
+  console.log(`application started on http://localhost:${app.get("port")}`);
+});
