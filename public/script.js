bb485eb968a0ee6ef902c57e834c4e01816aaf71
Sascha-davidson <saschadavidson1@gmail.com>
2023-06-11 00:18:15 +0200

    no sleef & full screen function

--- public/script.js
+++ public/script.js
@@ -1,18 +1,19 @@
-const buttons = document.querySelectorAll(`button`)
+const buttons = document.querySelectorAll(`button`);
 const menuBackground = document.querySelector(`body nav .background`);
 const background = document.querySelector(`body > .background`);
-const content = document.querySelector(`.content-container`)
-const displayTime = document.querySelector(`.display-time`)
+const content = document.querySelector(`.content-container`);
+const displayTime = document.querySelector(`.display-time`);
 
 let backgoundPositions = [0, 25, 50, 75, 100];
-let contentPositions = [-0, -20, -40, -60, -80]
+let contentPositions = [-0, -20, -40, -60, -80];
 let currentPosition = 0;
-let menuBackgroundPositions = [0, 100, 200, 300, 400]
+let menuBackgroundPositions = [0, 100, 200, 300, 400];
 
 var incomeTicker = 60;
+var noSleep = new NoSleep();
 
-buttons.forEach (button => button.hidden = false)
-displayTime.hidden = false
+buttons.forEach((button) => (button.hidden = false));
+displayTime.hidden = false;
 
 function NextPosition() {
   currentPosition += 1;
@@ -26,20 +27,44 @@ function SetPosition(newpostion) {
 }
 
 function UpdateBackgroundPosition() {
-  background.style.backgroundPosition = backgoundPositions[currentPosition] + `% ` + backgoundPositions[currentPosition] + `%`;
-  menuBackground.style.transform = "translateX(" + menuBackgroundPositions[currentPosition] + "%)";
-  content.style.transform = "translateX(" + contentPositions[currentPosition] + "%)";
+  background.style.backgroundPosition =
+    backgoundPositions[currentPosition] +
+    `% ` +
+    backgoundPositions[currentPosition] +
+    `%`;
+  menuBackground.style.transform =
+    "translateX(" + menuBackgroundPositions[currentPosition] + "%)";
+  content.style.transform =
+    "translateX(" + contentPositions[currentPosition] + "%)";
 }
 
 setInterval(() => {
   NextPosition();
 }, 60000);
 
-
 setInterval(() => {
-    if (incomeTicker > 0)
-    incomeTicker--;
-       document.getElementById("incomeTicker").innerHTML = "Next slide: " + incomeTicker + " seconds";
- if (incomeTicker <= 0)
-   incomeTicker = 60;
- }, 1000);
+  if (incomeTicker > 0) incomeTicker--;
+  document.getElementById("incomeTicker").innerHTML =
+    "Next slide: " + incomeTicker + " seconds";
+  if (incomeTicker <= 0) incomeTicker = 60;
+}, 1000);
+
+function enableNoSleep() {
+  noSleep.enable();
+  document.removeEventListener("touchstart", enableNoSleep, false);
+}
+
+// var elem = document.getElementById(`body`);
+// setInterval(() => {
+//   function openFullscreen() {
+//     if (elem.requestFullscreen) {
+//       elem.requestFullscreen();
+//     } else if (elem.webkitRequestFullscreen) {
+//       /* Safari */
+//       elem.webkitRequestFullscreen();
+//     } else if (elem.msRequestFullscreen) {
+//       /* IE11 */
+//       elem.msRequestFullscreen();
+//     }
+//   }
+// }, 10);
