dd74d278d58079fb72d98e9f7555d326424d3bd4
Sascha-davidson <saschadavidson1@gmail.com>
2023-06-14 14:44:00 +0200

    eerste api responsive gestijld (slijling moet nog better)

--- public/script.js
+++ public/script.js
@@ -5,7 +5,6 @@ const displayTime = document.querySelector(`.display-time`);
 const nav = document.querySelector(`header nav`);
 const nextSlide = document.querySelector(`.menu-text p`);
 const contentContainer = document.querySelector(`#content-container`);
-const h1 = document.querySelector(`h1`);
 
 let backgoundPositions = [0, 25, 50, 75, 100];
 let contentPositions = [-0, -20, -40, -60, -80];
@@ -13,10 +12,6 @@ let currentPosition = 0;
 let menuBackgroundPositions = [0, 100, 200, 300, 400];
 
 var incomeTicker = 60;
-
-if (h1){
-  h1.innerHTML = "Current page title";
-}
 
 if (contentContainer) {
   contentContainer.classList.add("content-container-js");
